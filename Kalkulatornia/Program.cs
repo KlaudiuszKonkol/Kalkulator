846a417fe6293e3f869efcf94f44f1a45154f770
KlaudiuszKonkol <klaudiuszkonkol3001@gmail.com>
2023-03-09 21:13:40 +0100

    move methods to Services.cs

--- Kalkulatornia/Program.cs
+++ Kalkulatornia/Program.cs
@@ -1,84 +1,24 @@
-﻿string Menu = "MENU Kalkulatora";
-string dodawanie = "Dodawanie - 1";
-string odejmowanie = "Odejmowanie - 2";
-string mnozenie = "Mnożenie - 3";
-string dzielenie = "Dzielenie - 4";
-string wyjscie = "Wyjście - 5";
-string skladnik = "Podaj pierwszy składnik";
-string skladnik2 = "Podaj drugi składnik";
-string wybor1 = "Wybrano Dodawanie " + skladnik;
-string wybor2 = "Wybrano Odejmowanie " + skladnik;
-string wybor3 = "Wybrano Mnożenie " + skladnik;
-string wybor4 = "Wybrano Dzielenie " + skladnik;
-string wynik = "Twój wynik to: ";
+﻿using Kalkulatornia;
 
-void wyborJeden()
-{
-    Console.WriteLine(wybor1);
-    int liczba1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine(skladnik2);
-    int liczba2 = Convert.ToInt32(Console.ReadLine());
-    int suma = liczba1 + liczba2;
-    Console.WriteLine(wynik + suma);
-}
-
-void wyborDwa()
-{
-    Console.WriteLine(wybor2);
-    int liczba1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine(skladnik2);
-    int liczba2 = Convert.ToInt32(Console.ReadLine());
-    int różnica = liczba1 - liczba2;
-    Console.WriteLine(wynik + różnica);
-}
-
-void wyborTrzy()
-{
-    Console.WriteLine(wybor3);
-    int liczba1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine(skladnik2);
-    int liczba2 = Convert.ToInt32(Console.ReadLine());
-    int iloczyn = liczba1 * liczba2;
-    Console.WriteLine(wynik + iloczyn);
-}
-
-void wyborCztery()
-{
-    Console.WriteLine(wybor4);
-    int liczba1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine(skladnik2);
-    int liczba2 = Convert.ToInt32(Console.ReadLine());
-    int iloraz = liczba1 / liczba2;
-    Console.WriteLine(wynik + iloraz);
-}
-
-void menu()
-{
-    Console.WriteLine(Menu);
-    Console.WriteLine(dodawanie);
-    Console.WriteLine(odejmowanie);
-    Console.WriteLine(mnozenie);
-    Console.WriteLine(dzielenie);
-    Console.WriteLine(wyjscie);
-}
+Services services = new Services();
 
 while (true)
 {
-    menu();
+    services.menu();
     string input = Console.ReadLine();
     switch (input)
     {
         case "1":
-            wyborJeden();
+            services.wyborJeden();
             break;
         case "2":
-            wyborDwa();
+            services.wyborDwa();
             break;
         case "3":
-            wyborTrzy();
+            services.wyborTrzy();
             break;  
         case "4":
-            wyborCztery();
+            services.wyborCztery();
             break;
         case "5":
             goto exit_loop;
---
+++ Kalkulatornia/Services.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulatornia
+{
+    public class Services
+    {
+        string Menu = "MENU Kalkulatora";
+        string dodawanie = "Dodawanie - 1";
+        string odejmowanie = "Odejmowanie - 2";
+        string mnozenie = "Mnożenie - 3";
+        string dzielenie = "Dzielenie - 4";
+        string wyjscie = "Wyjście - 5";
+        string skladnik2 = "Podaj drugi składnik";
+        string wybor1 = "Wybrano Dodawanie " + "Podaj pierwszy składnik";
+        string wybor2 = "Wybrano Odejmowanie " + "Podaj pierwszy składnik";
+        string wybor3 = "Wybrano Mnożenie " + "Podaj pierwszy składnik";
+        string wybor4 = "Wybrano Dzielenie " + "Podaj pierwszy składnik";
+        string wynik = "Twój wynik to: ";
+
+       public void wyborJeden()
+        {
+            Console.WriteLine(wybor1);
+            int liczba1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(skladnik2);
+            int liczba2 = Convert.ToInt32(Console.ReadLine());
+            int suma = liczba1 + liczba2;
+            Console.WriteLine(wynik + suma);
+        }
+
+        public void wyborDwa()
+        {
+            Console.WriteLine(wybor2);
+            int liczba1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(skladnik2);
+            int liczba2 = Convert.ToInt32(Console.ReadLine());
+            int różnica = liczba1 - liczba2;
+            Console.WriteLine(wynik + różnica);
+        }
+
+        public void wyborTrzy()
+        {
+            Console.WriteLine(wybor3);
+            int liczba1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(skladnik2);
+            int liczba2 = Convert.ToInt32(Console.ReadLine());
+            int iloczyn = liczba1 * liczba2;
+            Console.WriteLine(wynik + iloczyn);
+        }
+
+        public void wyborCztery()
+        {
+            Console.WriteLine(wybor4);
+            int liczba1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(skladnik2);
+            int liczba2 = Convert.ToInt32(Console.ReadLine());
+            int iloraz = liczba1 / liczba2;
+            Console.WriteLine(wynik + iloraz);
+        }
+
+        public void menu()
+        {
+            Console.WriteLine(Menu);
+            Console.WriteLine(dodawanie);
+            Console.WriteLine(odejmowanie);
+            Console.WriteLine(mnozenie);
+            Console.WriteLine(dzielenie);
+            Console.WriteLine(wyjscie);
+        }
+
+    }
+}
